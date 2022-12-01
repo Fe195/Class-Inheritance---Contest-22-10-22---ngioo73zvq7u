593e0531b2f6a09c6a6c6e3fb18cc7c4b96e5fcd
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-01 23:47:10 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
-// declare class
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+
+    }
+}
 
 // export class using module.exports
+module.exports = Shape;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
